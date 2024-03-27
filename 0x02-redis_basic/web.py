80dc42f4555dc0288ea7f66aee15f4525e1b442a
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2024-03-27 19:58:58 +0200

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,7 +9,6 @@ from typing import Callable
 redis_store = redis.Redis()
 '''The module-level Redis instance.
 '''
-
 def data_cacher(method: Callable) -> Callable:
     '''Caches the output of fetched data.
     '''
@@ -26,7 +25,6 @@ def data_cacher(method: Callable) -> Callable:
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
-
 @data_cacher
 def get_page(url: str) -> str:
     '''Returns the content of a URL after caching the request's response,
